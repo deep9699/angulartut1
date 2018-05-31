f7c7579dd07167ae46dd800c688345b22b23afc9
deep9699 <deepbhavsar9@gmail.com>
2018-05-31 10:23:32 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { CalcComponent } from './calc/calc.component';
+import { ArrdemoComponent } from './arrdemo/arrdemo.component';
+import { TodoComponent } from './todo/todo.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    CalcComponent,
+    ArrdemoComponent,
+    TodoComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/product/product.ts
@@ -0,0 +1,7 @@
+export class product
+{
+    constructor(public id:number,public name:string,public price:number,public quantity:number,public status:string){
+
+    }
+
+}
